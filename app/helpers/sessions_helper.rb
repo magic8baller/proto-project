4a8e08b32a4869a4517d983c1c85f52d01c56212
magic8baller <lturis@gmail.com>
2018-11-11 15:59:33 -0500

    more advanced login, unsure about questionmarks in creating ?user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,10 +6,17 @@ module SessionsHelper
   end
   #temporary cookies created using the session method are automatically encrypted-code is secure
 
-  # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      raise       # The tests still pass, so this branch is currently untested.
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -18,8 +25,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
