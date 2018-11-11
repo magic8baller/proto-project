7f060d0b6e508d4fbe497c801ae2375a69bc7d2e
magic8baller <lturis@gmail.com>
2018-11-11 15:39:57 -0500

    basic login stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user #auto converts to route for user profile
+    else
+      # Create an error message: .now vs flash[:errors]:
+      #contents of flash.now disappear as soon as there is an additional request
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,14 @@ class UsersController < ApplicationController
   end
 
   def create
-       @user = User.create(user_params)
-
+    @user = User.create(user_params)
+    if @user.valid?
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
 
