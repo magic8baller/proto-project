887a45d5c7140753c9d820eb9f80ac974cffe24d
magic8baller <lturis@gmail.com>
2018-11-12 19:23:01 -0500

    o

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
   root 'home#index'
   get  '/help',    to: 'home#help'
   get  '/about',   to: 'home#about'
   get  '/contact', to: 'home#contact'
+  get '/users', to: 'users#index'
   get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get '/users', to: 'users#index'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
